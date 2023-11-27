5b7feb9f4d5f3319ce26fa79e33fb69b97b95580
Estefanny-Tucuch <143752773+Estefanny-Tucuch@users.noreply.github.com>
2023-11-27 01:09:08 -0600

    Repertorio

--- Frontend/Proyecto/src/app/pages/repertorio/repertorio.component.ts
+++ Frontend/Proyecto/src/app/pages/repertorio/repertorio.component.ts
@@ -8,100 +8,98 @@ import { Component } from '@angular/core';
 export class RepertorioComponent {
   searchText: string = '';
   songs: string[] = [
-    'A La Sombra De Mamá',
-    'Abuela',
-    'A Esa Mujer',
+    'Las Mañanitas',
+    'Pelo Blanco',
+    'Hermoso Cariño',
+    'Cariñito',
+    'Gema',
+    'Reyna De Reynas',
+    'Madrecita De Mi Alma',
     'Amor Eterno',
-    'Aunque No Sea Mayo',
-    'Clavitos con Amor',
-    'Canto A la madre',
-    'Dulce Hogar',
-    'Entre El Amor Y YO',
-    'Es La Mujer',
-    'El Camino De La Vida',
-    'Es Mi Madre',
-    'En mi vida',
-    'Los Caminos De La Vida',
-    'La Nieve De Los Años',
-    'Mama',
+    'Señora, Señora',
+    'Hasta Que Te Conoci',
+    'En Tu Dia',
+    '100 Años',
     'Motivos',
+    'En Mi Vida',
+    'Mujeres Divinas',
+    'Cielito Lindo',
+    'Mama',
     'Madrecita Ideal',
     'Mama Vieja',
-    'Madre del Corazon'
+    'Madre Del Corazon'
 
     // Agrega más canciones aquí
   ];
   songs2: string[] = [
-    'Amigo',
-    'A Mis Amigos',
-    'Bohemio De Afición',
-    'Camino Al Cielo',
-    'Cuando Yo Quería Ser Grande',
-    'Dulce Hogar',
-    'El Aventurero',
-    'El Bravero',
-    'El Camino De La Vida',
-    'El Hijo Desobediente',
-    'El Hijo Del Pueblo',
-    'El Hombre Que Más Te Amo',
     'El Rey',
-    'La Ley De La Vida',
-    'La Nieve De Los Años',
+    'Amigo',
+    'Mi Querido Viejo',
+    'Como Quisisera Ser Grande',
+    'Volver, Volver',
+    'Puño De Tierra',
+    'El Aventurero',
+    'El Hijo Desobediente',
+    'Camino De Michoacan',
+    'Sangre En Mi Cuerpo',
+    'De Que Manera Te Olvido',
+    'Aca Entre Nos',
+    'Cumpleaños',
+    'Por Tu Maldito Amor',
+    'Triste Recuerdo',
     'Mi Querido Cascarrabias',
-    'Mi Querido Mi Viejo Mi Amigo',
-    'Mi Vejez',
+    'La Ley Del Monte',
     'Mi Viejo',
+    'Mi Vejez',
 
     // Agrega más canciones aquí
   ];
   songs3: string[] = [
-    'Amigo Roberto Carlos',
-    'El Camino De La Vida',
-    'En Vida',
-    'En Tu Día',
-    'Happy Birthday',
-    'La Nieve De Los Años',
-    'Los Caminos De La Vida',
-    'Nochecitas Mexicanas',
-    'Las Mañanitas Rey David',
-    'Las Mañanitas Mexicanas',
-    'Por Tu Cumpleaños',
-    'Sabes Una Cosa',
-    'En Tu Cumpleaños',
-    'Un Año Más',
+    'Felicidades',
+    'Celebremos',
+    'Mariachi Loco',
+    'Mono De Alambre',
+    'Guadalajara',
+    'El Son De La Negra',
+    'Jarabe Tapatio',
+    'Pelea De Gallos',
+    'Despacito',
+    'El Noa Noa',
+    'El Cable',
+    'El Huitlacoche',
+    'No se Va',
   ];
   songs4: string[] = [
-    'La Mano de Dios',
-    'Las Llaves De Mi Alma',
-    'La Venia Bendita',
-    'Madrigal',
-    'Mi Forma de Sentir',
-    'Mujeres Divinas',
-    'Malagueña',
-    'Más Que Tu Amigo',
-    'Mátalas',
-    'Motivos',
-    'Nuestro Juramento',
-    'Nos Estorbo La Ropa',
-    'Novia Mia',
-    'Obsesion',
-    'Para Siempre',
-    'Piel De Niña',
+    'Diseñame',
+    'Tatuajes',
+    'Si Nos Dejan',
+    'La Media Vuelta',
+    'Serenata Huasteca',
+    'Mas Que Tu Amigo',
+    'Amarte A La Antigua',
+    'Para Que Me Haces Llorar',
+    'Urge',
+    'Matalas',
+    'Un Millon De Primaveras',
+    'Cielo Rojo',
+    'Por Mujeres Como Tu',
+    'Estos Celos',
+    'Ella',
+    'La Bikina',
     'Paloma Querida',
-    'Parece Que Fue Ayer',
-    'Toda Una Vida',
-    'Paso a La Reina',
-    'Que Se Mueran De Envidia',
+    'Fallaste Corazon',
+    'Como Quien Pierda Una Estrella',
+    'Sabes Una Cosa',
   ];
   isListExpanded: boolean = true;
   isListExpanded1: boolean = true;
   isListExpanded2: boolean = false;
   isListExpanded3: boolean = false;
-  
+
   toggleList1() {
     this.isListExpanded = !this.isListExpanded;
   }
-  
+
   toggleList2() {
     this.isListExpanded1 = !this.isListExpanded1;
   }
@@ -111,5 +109,5 @@ export class RepertorioComponent {
   toggleList4() {
     this.isListExpanded3  = !this.isListExpanded3 ;
   }
-   
+
 }
